3734dc5dc5d12ad3bb89ee5e57a4de30be7fbced
YAJATapps <yajatapps3@gmail.com>
2024-08-21 15:25:56 -0400

    Remove updater

--- public/package/apk/Update/bbcCoreSdk/src/main/java/com/bigbigcloud/devicehive/message/MessageReciever.java
+++
@@ -1,9 +0,0 @@
-package com.bigbigcloud.devicehive.message;
-
-/**
- * Created by Administrator on 2016/2/18.
- */
-public interface MessageReciever {
-
-    public void onReceive(BaseMessage baseMessage);
-}
